1d4ecbdaec60493845d945f46f4064611eb2c260
DJombo <163758588+DJombo@users.noreply.github.com>
2024-05-21 20:27:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var ctx = document.getElementById('chart').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    }
+});
+
+var ctx = document.getElementById('myDoughnutChart').getContext('2d');
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+});
+
+
+function switchTheme() {
+    var theme1 = document.getElementById('theme1');
+    var theme2 = document.getElementById('theme2');
+    if (theme1.disabled) {
+        theme1.disabled = false;
+        theme2.disabled = true;
+    } else {
+        theme1.disabled = true;
+        theme2.disabled = false;
+    }
+}
+
+
+function handleSearch() {
+    var searchBar = document.getElementById('searchBar');
+    var searchText = searchBar.value;
+    
+}
